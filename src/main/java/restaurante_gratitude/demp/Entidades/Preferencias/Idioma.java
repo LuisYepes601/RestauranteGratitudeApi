5e9804a59b41a5425a72e3fa5a67555067f195a4
Luis <yepesluis006@gmail.com>
2025-09-11 16:01:49 -0500

    creacion_entidades_sin_relacion_v1.3

---
+++ src/main/java/restaurante_gratitude/demp/Entidades/Preferencias/Idioma.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package restaurante_gratitude.demp.Entidades.Preferencias;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ *
+ * @author User
+ */
+@Entity
+public class Idioma {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(nullable = false)
+    private String nombre;
+
+    public Idioma(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Idioma() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
