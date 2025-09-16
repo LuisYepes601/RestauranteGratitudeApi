93c86059d2302b088c8574b550f39b2f933faa36
Luis <yepesluis006@gmail.com>
2025-09-16 10:07:22 -0500

    capa repositorios v2.2.paquete_direccion

---
+++ src/main/java/restaurante_gratitude/demp/Repositorys/Devoluciones/Reembolso/TipoReembolsoRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package restaurante_gratitude.demp.Repositorys.Devoluciones.Reembolso;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import restaurante_gratitude.demp.Entidades.Devoluciones.Reembolso.TipoReembolso;
+
+/**
+ *
+ * @author User
+ */
+public interface TipoReembolsoRepository extends JpaRepository<TipoReembolso, Integer>{
+    
+}
---
+++ src/main/java/restaurante_gratitude/demp/Repositorys/Direccion/DepartamentoRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package restaurante_gratitude.demp.Repositorys.Direccion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import restaurante_gratitude.demp.Entidades.Direccion.Departamento;
+
+/**
+ *
+ * @author User
+ */
+@Repository
+public interface DepartamentoRepository extends JpaRepository<Departamento, Integer> {
+
+}
