6f3b5bd6eb059480cc0ec3cc368707c328d7f05e
Luis <yepesluis006@gmail.com>
2025-09-16 11:20:26 -0500

    capa repositorios v2.3.paquete_contacatanos

---
+++ src/main/java/restaurante_gratitude/demp/Repositorys/Contactanos/RespuestaContactanosRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package restaurante_gratitude.demp.Repositorys.Contactanos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import restaurante_gratitude.demp.Entidades.Contactanos.RespuestaContactanos;
+
+/**
+ *
+ * @author User
+ */
+@Repository
+public interface RespuestaContactanosRepository extends JpaRepository<RespuestaContactanos, Integer>{
+    
+    
+    
+}
