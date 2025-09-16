9d15b94cbfde56658751a8fe0d4d7866bdb2ff9f
Luis <yepesluis006@gmail.com>
2025-09-16 11:04:58 -0500

    capa repositorios v2.3.paquete_contratos

---
+++ src/main/java/restaurante_gratitude/demp/Repositorys/Contratos/ContratoRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package restaurante_gratitude.demp.Repositorys.Contratos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import restaurante_gratitude.demp.Entidades.Contratos.Contrato;
+
+/**
+ *
+ * @author User
+ */
+@Repository
+public interface ContratoRepository extends JpaRepository<Contrato, Integer>{
+    
+}
---
+++ src/main/java/restaurante_gratitude/demp/Repositorys/Contratos/TipoContratoRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package restaurante_gratitude.demp.Repositorys.Contratos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import restaurante_gratitude.demp.Entidades.Contratos.TipoContrato;
+
+/**
+ *
+ * @author User
+ */
+public interface TipoContratoRepository extends JpaRepository<TipoContrato, Integer>{
+    
+}
