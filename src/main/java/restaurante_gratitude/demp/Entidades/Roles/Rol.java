e151d17a6ef416f9c6bbb7837c548a1edf7a7012
LuisYepes601 <tucorreo@example.com>
2026-06-08 15:07:30 -0500

    configurando_entidad_rol

--- src/main/java/restaurante_gratitude/demp/Entidades/Roles/Rol.java
+++ src/main/java/restaurante_gratitude/demp/Entidades/Roles/Rol.java
@@ -9,14 +9,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.hibernate.annotations.UpdateTimestamp;
 import restaurante_gratitude.demp.Entidades.Usuarios.Usuario;
 
 /**
  *
  * @author Usuario
  */
+@Table(name = "rol",
+        indexes = {
+            @Index(name = "idx_unique_rol_nombre", unique = true, columnList = "nombre"),
+            @Index(name = "idx_rol_codigo_rol", columnList = "codigoRol")
+
+        })
 @Entity
 public class Rol {
 
@@ -32,11 +42,31 @@ public class Rol {
     @OneToMany(mappedBy = "rol")
     private List<Usuario> usuarios;
 
-    public Rol(int id, String nombre, String codigoRol, List<Usuario> usuarios) {
+    @Column(nullable = false,
+            name = "is_delete")
+    private boolean isDelete;
+
+    @Column(nullable = false,
+            name = "create_at")
+    private LocalDateTime createAt;
+
+    @Column(nullable = false,
+            name = "update_at")
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+
+    @Column(name = "delete_at")
+    private LocalDateTime deleteAt;
+
+    public Rol(int id, String nombre, String codigoRol, List<Usuario> usuarios, boolean isDelete, LocalDateTime createAt, LocalDateTime updateAt, LocalDateTime deleteAt) {
         this.id = id;
         this.nombre = nombre;
         this.codigoRol = codigoRol;
         this.usuarios = usuarios;
+        this.isDelete = isDelete;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.deleteAt = deleteAt;
     }
 
     public Rol() {
@@ -72,6 +102,38 @@ public class Rol {
 
     public void setCodigoRol(String codigoRol) {
         this.codigoRol = codigoRol;
+    }
+
+    public boolean isIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(boolean isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public LocalDateTime getDeleteAt() {
+        return deleteAt;
+    }
+
+    public void setDeleteAt(LocalDateTime deleteAt) {
+        this.deleteAt = deleteAt;
     }
 
 }
