9e7445f3ac125c57f922d34c460a3e0a05abbb2f
LuisYepes601 <tucorreo@example.com>
2026-07-12 18:23:09 -0500

    config sexodtoRequestv:1

--- src/main/java/restaurante_gratitude/demp/DTOS/Request/Sexo/SexoDto.java
+++ src/main/java/restaurante_gratitude/demp/DTOS/Request/Sexo/SexoDto.java
@@ -4,7 +4,9 @@
  */
 package restaurante_gratitude.demp.DTOS.Request.Sexo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 /**
  *
@@ -12,8 +14,26 @@ import jakarta.validation.constraints.NotBlank;
  */
 public class SexoDto {
 
+    @Schema(
+            name = "nombre",
+            example = "Femenino",
+            requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank(message = "El campo nombre de la entidad sexo, no puede quedar vacio lo invitamos a rellenarlo")
+    @Size(
+            min = 3,
+            max = 20,
+            message = "El nombre del Sexo debe de tener al menos 3 caracteres y un máximo de 20.")
     private String nombre;
+
+    @Schema(
+            name = "description",
+            example = "El sexo femenino respresnta a las personas cisgeneros",
+            requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @Size(
+            min = 10,
+            max = 200,
+            message = "La descricción del sexo debe de tener al menos 10 caracteres y máximo 200")
+    private String description;
 
     public SexoDto(String nombre) {
         this.nombre = nombre;
